0a8e88750e648342e158dfee6243f8e568354797
Elizabeth Emig <epitman523@gmail.com>
2019-08-06 15:25:58 -0500

    finished js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,20 @@
 'use strict';
 
-
-//3) get number user entered
-//4) add number to end of url string
-//5) Use fetch to access Dog API
-//6) Console.log returned images
-
-
+function getDogImages() {
+    //3) get number user entered
+    let userNumber = $('input').val();
+    if (!userNumber) {
+        userNumber = 3;
+    }
+    //4) add number to end of url string
+    let doggieURL = 'https://dog.ceo/api/breeds/image/random/' + userNumber;
+    //5) Use fetch to access Dog API
+    fetch(doggieURL)
+        .then(response => response.json())
+        //6) Console.log returned images
+        .then(responseJSON => console.log(responseJSON))
+        .catch(error => console.log('Something went wrong'));
+}
 
 //1) User submits form
 function watchDogForm() {
